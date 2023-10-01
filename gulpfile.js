71eb2889c46663270478d1b25c26f0662596c5bd
Marcelo Dantas <146590378+devMahaw@users.noreply.github.com>
2023-10-01 12:05:58 -0300

    style: unnecessary variable removed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const uglify = require("gulp-uglify");
-const obfuscate = require("gulp-obfuscate");
 const imagemin = require("gulp-imagemin");
 
 function comprimeImagens() {
